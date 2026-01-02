c64fdf07b0b7a2d591fa68bad63cbedf7cff4bcc
Vincent Hanquez <vincent@coretype.io>
2026-01-02 17:18:15 +0800

    improvement of ast

--- crates/skelm-exec/src/template/jinja/ast.rs
+++ crates/skelm-exec/src/template/jinja/ast.rs
@@ -2,21 +2,21 @@ use super::block::Block;
 use super::parser::{ParseToken, Statement};
 
 #[derive(Debug)]
-pub enum StatementAst {
+pub enum StatementAst<'a> {
     If {
-        conditions: Vec<(Expression, StatementBlock)>,
+        conditions: Vec<(Expression, StatementBlock<'a>)>,
         else_: Option<()>,
     },
     For {
         binder: Binder,
         iterator: Expression,
-        body: StatementBlock,
+        body: StatementBlock<'a>,
     },
-    Expression(Expression),
+    Expression(&'a Expression),
     Statement(Statement),
 }
 
-pub type StatementBlock = Vec<StatementAst>;
+pub type StatementBlock<'a> = Vec<Block<StatementAst<'a>>>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Operator {
@@ -61,38 +61,61 @@ pub enum ArraySubexpr {
     Range(Option<String>, Option<String>),
 }
 
-pub enum AstBuilder {
+pub enum AstBuilder<'a> {
     If {
-        conditions: Vec<(Expression, StatementBlock)>,
+        conditions: Vec<(&'a Expression, StatementBlock<'a>)>,
     },
     IfElse {
-        conditions: Vec<(Expression, StatementBlock)>,
-        else_: StatementBlock,
+        conditions: Vec<(&'a Expression, StatementBlock<'a>)>,
+        else_: StatementBlock<'a>,
     },
     For {
-        binder: Binder,
-        body: Box<AstBuilder>,
+        binder: &'a Binder,
+        body: StatementBlock<'a>,
     },
 }
 
-pub fn ast<'a>(blocks: &[Block<ParseToken<'a>>]) -> Vec<StatementAst> {
-    let mut stack: Vec<StatementBlock> = Vec::new();
+impl<'a> AstBuilder<'a> {}
+
+pub fn ast<'a>(blocks: &[Block<ParseToken<'a>>]) -> Vec<Block<StatementAst<'a>>> {
+    let mut stack: Vec<AstBuilder<'_>> = Vec::new();
 
     let mut blocks = blocks.iter();
     while let Some(block) = blocks.next() {
         match &block.content {
             ParseToken::Statement(statement) => {
                 match statement {
-                    Statement::If { condition } => todo!(),
-                    Statement::Elif { condition } => todo!(),
-                    Statement::For { bind, iter } => todo!(),
+                    Statement::If { condition } => {
+                        // push a new builder
+                        stack.push(AstBuilder::If {
+                            conditions: vec![(condition, vec![])],
+                        });
+                    }
+                    Statement::Elif { condition } => {
+                        //
+                        match stack.last_mut() {
+                            Some(AstBuilder::If { conditions }) => {
+                                conditions.push((condition, vec![]))
+                            }
+                            Some(_) => {
+                                // elif is a non if context, TODO create an error
+                                todo!()
+                            }
+                            None => {
+                                // should not happen
+                                todo!()
+                            }
+                        };
+                    }
+                    Statement::For { bind, iter } => stack.push(AstBuilder::For {
+                        binder: bind,
+                        body: vec![],
+                    }),
                     Statement::Set { expr, value } => todo!(),
                     Statement::EndIf => todo!(),
                     Statement::Else => todo!(),
                     Statement::EndFor => todo!(),
                 }
-                //
-                todo!()
             }
             ParseToken::Expression(expression) => {
                 //
